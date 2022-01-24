2b2367e2687e917ee8898a246e3bd6240adfcf2c
Zarif <zarifarefeen@gmail.com>
2022-01-24 16:36:13 +0600

    New form try

---
+++ lib/module/resume_Create/screen/resumeForm/project.dart
@@ -0,0 +1,222 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:resume_maker/common/theme/appTheme.dart';
+import 'package:resume_maker/common/widget/date_input_field.dart';
+import 'package:resume_maker/common/widget/mutliline_textField.dart';
+import 'package:resume_maker/common/widget/textField.dart';
+
+class Project extends StatefulWidget {
+  const Project({Key? key}) : super(key: key);
+
+  @override
+  _ProjectState createState() => _ProjectState();
+}
+
+class _ProjectState extends State<Project> with AppTheme{
+  List<TextEditingController> _nameControllers = [];
+  List<TextFieldWidget> _nameList = [];
+  List<TextEditingController> _designationControllers = [];
+  List<TextFieldWidget> _designationList = [];
+  List<TextEditingController> _instituteContoller = [];
+  List<TextFieldWidget> _instituteList = [];
+  List<TextEditingController> _emailController = [];
+  List<TextFieldWidget> _emailList = [];
+  List<TextEditingController> _mobileController = [];
+  List<TextFieldWidget> _mobileList = [];
+
+
+  bool addTapped =  false;
+
+
+
+
+  void _addTile(String function) {
+
+    final name = TextEditingController();
+    final designation = TextEditingController();
+    final institute = TextEditingController();
+    final email = TextEditingController();
+    final mobile_no = TextEditingController();
+
+
+    final nameField = TextFieldWidget(controller: name, type: 'degree', hintText: 'Name',);
+    final designationField = TextFieldWidget(controller:designation, type: 'name', hintText: 'Designation',);
+    final instituteField = TextFieldWidget(controller: institute, type: 'number', hintText: 'Institute',);
+    final emailField = TextFieldWidget(controller: email, type: 'name', hintText: 'Email',);
+    final mobile_noField = TextFieldWidget(controller: mobile_no, type: 'name', hintText: 'Mobile Number',);
+
+    setState(() {
+      if(function == "add") {
+        _nameControllers.add(name);
+        _designationControllers.add(designation);
+        _instituteContoller.add(institute);
+        _emailController.add(email);
+        _mobileController.add(mobile_no);
+
+        _nameList.add(nameField);
+        _designationList.add(designationField);
+        _instituteList.add(instituteField);
+        _emailList.add(emailField);
+        _mobileList.add(mobile_noField);
+        addTapped = true;
+      }
+      // else{
+      //   _nameControllers.remove(name);
+      //   _telControllers.remove(tel);
+      //   _addressControllers.remove(address);
+      //   _nameFields.remove(nameField);
+      //   _telFields.remove(telField);
+      //   _addressFields.remove(addressField);
+      //   fieldCount--;
+      // }
+
+    });
+
+  }
+
+
+
+
+  Widget _listView() {
+    int? _value = 0;
+    bool active=false;
+    final children = [
+      for (var i = 0; i < _nameControllers.length; i++)
+        Container(
+
+          child: Column(
+            children: [
+              Text('${i+1}'),
+
+              _nameList[i],
+              _designationList[i],
+              _instituteList[i],
+              _emailList[i],
+              _mobileList[i],
+
+
+              SizedBox(height: size.s20,)
+            ],
+          ),
+        )
+    ];
+    return Container(
+      child: Column(
+        children: children,
+      ),
+    );
+  }
+  bool _isShowing= false;
+  void _toggleViewer() {
+    setState(() {
+      _isShowing = !_isShowing;
+    });
+  }
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 2.w),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: _toggleViewer,
+            child: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: clr.appBlack,
+                    borderRadius: BorderRadius.circular(50.r),
+
+                  ),
+                  child: _isShowing?Icon(Icons.check,color: Colors.white,):Icon(Icons.school,color: Colors.white,),
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: size.s12),
+                    padding: EdgeInsets.symmetric(vertical: size.s12,),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:16.0),
+                      child: Text(
+                        "Reference",
+                        style: TextStyle(
+                          // color:widget.service.currentChapterId == widget.chapter.id ?Colors.orange:Colors.black.withOpacity(.7),
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(size.s8),
+                  child: Icon(_isShowing?Icons.keyboard_arrow_up:Icons.keyboard_arrow_down, size: 26, color: Colors.grey.withOpacity(.9),),
+                ),
+              ],
+
+            ),
+          ),
+          _isShowing?Container(
+            child: Column(
+              children: [
+                !addTapped? Text("Tap the ""+"" button to add your Projects",
+                  style: TextStyle(color: clr.appBlack),
+                ):Offstage(),
+                SizedBox(height: size.s20,),
+                _listView(),
+
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+
+
+
+                      GestureDetector(
+                        onTap: ()=>_addTile('add'),
+                        child: Container(
+                            margin: EdgeInsets.only(bottom: size.s8,right: size.s4),
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: clr.appBlack,
+                              borderRadius: BorderRadius.circular(50.r),
+
+                            ),
+                            child:Icon(Icons.add,color: Colors.white,)
+                        ),
+                      ),
+
+                      // GestureDetector(
+                      //   onTap: ()=>_addTile('remove'),
+                      //   child: Container(
+                      //       margin: EdgeInsets.only(bottom: size.s8,right: size.s4),
+                      //       padding: EdgeInsets.all(8),
+                      //       decoration: BoxDecoration(
+                      //         color: clr.appBlack,
+                      //         borderRadius: BorderRadius.circular(50.r),
+                      //
+                      //       ),
+                      //       child:Icon(Icons.delete,color: Colors.white,)
+                      //   ),
+                      // ),
+                    ]
+                ),
+              ],
+            ),
+          ):Offstage(),
+
+
+          Container(
+            height: 1.2,
+            width: MediaQuery.of(context).size.width -100,
+            color: Colors.grey.withOpacity(.25),
+          ),
+
+          // Divider(thickness: 3,indent: 30,endIndent: 30,)
+        ],
+      ),
+    );
+  }
+}
--- lib/module/resume_Create/screen/resume_create_screen.dart
+++ lib/module/resume_Create/screen/resume_create_screen.dart
@@ -2,16 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:im_stepper/stepper.dart';
-import 'package:resume_maker/common/core/app.dart';
 import 'package:resume_maker/common/model/Academics.dart';
 import 'package:resume_maker/common/theme/appTheme.dart';
 import 'package:resume_maker/common/widget/background.dart';
-import 'package:resume_maker/common/widget/mutliline_textField.dart';
-import 'package:resume_maker/common/widget/textField.dart';
+
 import 'package:resume_maker/module/resume_Create/screen/resumeForm/academic.dart';
-import 'package:resume_maker/module/resume_Create/screen/resumeForm/account.dart';
-import 'package:resume_maker/module/resume_Create/screen/resumeForm/experience.dart';
 
 class ResumeCreateScreen extends StatefulWidget {
   @override
@@ -29,27 +24,36 @@ class _ResumeCreateScreen extends State<ResumeCreateScreen> with AppTheme{
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: RoundedAppBar(
-        onBack: true,
-        title: 'Create Resume',
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              LinearContainer(),
-              UserCreds(),
-              LinearContainer(),
-              Academic(),
-              LinearContainer(),
-              Experience()
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.transparent, // navigation bar color
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,// status bar color
+    ));
+    return RoundedAppBar(
+      onBack: true,
+      title: 'Create Resume',
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            LinearContainer(),
+            FormContent(icon: Icons.person, title: "Account"),
+            LinearContainer(),
+            FormContent(icon: Icons.school, title: "Academic"),
+            LinearContainer(),
+            FormContent(icon: Icons.work, title: "Experience"),
+            LinearContainer(),
+            FormContent(icon: Icons.assignment, title: "Project"),
+            LinearContainer(),
+            FormContent(icon: Icons.connect_without_contact, title: "Reference"),
+            LinearContainer(),
+            FormContent(icon: Icons.camera, title: "Camera"),
+            LinearContainer(),
+            FormContent(icon: Icons.gesture, title: "Signature"),
 
-
-            ],
-          ),
-        )
+          ],
+        ),
       )
-
     );
 
   }
@@ -63,9 +67,93 @@ class LinearContainer extends StatelessWidget with AppTheme{
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.only(left: 18.w),
-      height: size.s42,
+      height: size.s64,
       width: 3.w,
       color: clr.appBlack,
+    );
+  }
+}
+
+class FormContent extends StatefulWidget {
+  final IconData icon;
+  final String title;
+  const FormContent({Key? key, required this.icon, required this.title}) : super(key: key);
+
+  @override
+  _FormContentState createState() => _FormContentState();
+}
+
+class _FormContentState extends State<FormContent>with AppTheme {
+
+  bool _isShowing= false;
+  void _toggleViewer() {
+    setState(() {
+      _isShowing = !_isShowing;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(left: 2.w),
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: _toggleViewer,
+            child: Row(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: clr.appBlack,
+                    borderRadius: BorderRadius.circular(50.r),
+
+                  ),
+                  child:Icon(widget.icon,color: Colors.white,)
+                ),
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.only(left: size.s12),
+                    padding: EdgeInsets.symmetric(vertical: size.s12,),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal:16.0),
+                      child: Text(
+                        widget.title,
+                        style: TextStyle(
+                          // color:widget.service.currentChapterId == widget.chapter.id ?Colors.orange:Colors.black.withOpacity(.7),
+                          fontSize: 19,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(size.s8),
+                  child: Icon(_isShowing?Icons.keyboard_arrow_up:Icons.keyboard_arrow_down, size: 26, color: Colors.grey.withOpacity(.9),),
+                ),
+              ],
+
+            ),
+          ),
+          // _isShowing?Container(
+          //   child: Column(
+          //     children: [
+          //       Academics()
+          //       ]
+          //   ),
+          // ):Offstage(),
+          Academic(),
+          Container(
+            height: 1.2,
+            width: MediaQuery.of(context).size.width -100,
+            color: Colors.grey.withOpacity(.25),
+          ),
+
+          // Divider(thickness: 3,indent: 30,endIndent: 30,)
+        ],
+      ),
     );
   }
 }
